d79d5f5f9207b0aafdab8a9a57b1a87f88755f46
CJ Williams <cjwilliams@CJs-MacBook-Pro.local>
2024-04-01 17:39:39 -0400

    Starting level 2.

--- level.py
+++ level.py
@@ -56,3 +56,20 @@ level1 = Level(
         Star(x=SCREEN_WIDTH*0.9, width=SCREEN_WIDTH*.05, y=SCREEN_HEIGHT*0.85, height=SCREEN_HEIGHT*.05,),
     ],
 )
+level2 = Level(
+    surfaces=[
+        #ground surface
+        Surface(x=0,  width=SCREEN_WIDTH, y=SCREEN_HEIGHT*.95, height=SCREEN_HEIGHT),
+        #floating platfrom in center
+        #the formula in X ensures its centered, so long as the width is using the same value
+        Surface(x=(SCREEN_WIDTH - (SCREEN_WIDTH * 0.3)) / 2, width=SCREEN_WIDTH*.3, y=SCREEN_HEIGHT*.8, height=SCREEN_HEIGHT*.05),
+    ],
+    lava=[
+        #lava pit in center
+        Lava(x=(SCREEN_WIDTH - (SCREEN_WIDTH * 0.4)) / 2, width=SCREEN_WIDTH*.4, y=SCREEN_HEIGHT*.95, height=SCREEN_HEIGHT),
+    ],
+    stars=[
+        #star on the right side
+        Star(x=SCREEN_WIDTH*0.9, width=SCREEN_WIDTH*.05, y=SCREEN_HEIGHT*0.85, height=SCREEN_HEIGHT*.05,),
+    ],
+)
