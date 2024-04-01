b8c121ef38b73943a41dc635ed5632724a0e7e6c
CJ Williams <cjwilliams@CJs-MacBook-Pro.local>
2024-04-01 18:17:57 -0400

    Working on level2

--- level.py
+++ level.py
@@ -58,18 +58,24 @@ level1 = Level(
 )
 level2 = Level(
     surfaces=[
-        #ground surface
-        Surface(x=0,  width=SCREEN_WIDTH, y=SCREEN_HEIGHT*.95, height=SCREEN_HEIGHT),
-        #floating platfrom in center
-        #the formula in X ensures its centered, so long as the width is using the same value
-        Surface(x=(SCREEN_WIDTH - (SCREEN_WIDTH * 0.3)) / 2, width=SCREEN_WIDTH*.3, y=SCREEN_HEIGHT*.8, height=SCREEN_HEIGHT*.05),
+        # ground surface
+        Surface(x=0, width=SCREEN_WIDTH, y=SCREEN_HEIGHT*.95, height=SCREEN_HEIGHT),
+        # floating platform on the left
+        Surface(x= 5 , width=SCREEN_WIDTH * 0.4, y=SCREEN_HEIGHT * 0.7, height=SCREEN_HEIGHT * 0.05),
+
+        # floating platform on the right
+        Surface(x=SCREEN_WIDTH * 0.6, width=SCREEN_WIDTH * 0.3, y=SCREEN_HEIGHT * 0.6, height=SCREEN_HEIGHT * 0.05),
     ],
     lava=[
-        #lava pit in center
-        Lava(x=(SCREEN_WIDTH - (SCREEN_WIDTH * 0.4)) / 2, width=SCREEN_WIDTH*.4, y=SCREEN_HEIGHT*.95, height=SCREEN_HEIGHT),
+        # lava pit at the beginning
+        Lava(x=0, width=SCREEN_WIDTH * 0.2, y=SCREEN_HEIGHT * 0.95, height=SCREEN_HEIGHT),
+        # lava pit on the left
+        Lava(x=0, width=SCREEN_WIDTH * 1, y=SCREEN_HEIGHT * 0.95, height=SCREEN_HEIGHT),
+        # lava pit on the right
+        Lava(x=SCREEN_WIDTH * 0.9, width=SCREEN_WIDTH * 0.1, y=SCREEN_HEIGHT * 0.95, height=SCREEN_HEIGHT),
     ],
     stars=[
-        #star on the right side
-        Star(x=SCREEN_WIDTH*0.9, width=SCREEN_WIDTH*.05, y=SCREEN_HEIGHT*0.85, height=SCREEN_HEIGHT*.05,),
-    ],
+        # star at the end of the right floating platform
+        Star(x=SCREEN_WIDTH * 0.9 - SCREEN_WIDTH * 0.05, width=SCREEN_WIDTH*0.05, y=SCREEN_HEIGHT*0.6, height=SCREEN_HEIGHT*0.05),
+]
 )
